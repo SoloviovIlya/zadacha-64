72970a229337cc1ae32fe84d7c658735f09716a4
dadaya <liola.soloviov@mail.ru>
2023-02-28 14:34:37 +0300

    comment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void NatChisla(int n)
+{
+   if (n==0) return;
+   Console.WriteLine($"{n} ");
+   NatChisla(n-1);
+}
+NatChisla(n);
+
